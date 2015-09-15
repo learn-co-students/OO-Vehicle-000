03be19e2f7c6e171b3661abb53ae664c7abd1cc9
dfenjves <fenjves.daniel@gmail.com>
2015-09-14 21:46:53 -0400

    Done.

--- OO-Vehicle/FISVehicle.m
+++ OO-Vehicle/FISVehicle.m
@@ -10,6 +10,34 @@
 
 @implementation FISVehicle
 
-//code goes here
+-(void)increaseSpeed{
+    self.currentSpeed = self.topSpeed;
+    
+}
+-(void)brake{
+    self.currentSpeed = 0;
+}
+-(void)turnLeft{
+    self.currentDirection = (self.currentDirection - 90);
+    if (self.currentDirection < 0) {
+        self.currentDirection += 360;
+    }
+}
+-(void)turnRight{
+    self.currentDirection = (self.currentDirection + 90);
+    if (self.currentDirection >= 360) {
+        self.currentDirection -= 360;
+    }
+}
+-(instancetype)initWithWeight:(CGFloat)weight topSpeed:(CGFloat)topSpeed currentSpeed:(CGFloat)currentSpeed currentDirection:(CGFloat)currentDirection{
+    self = [super init];
+    if (self) {
+        _weight = weight;
+        _topSpeed = topSpeed;
+        _currentSpeed = currentSpeed;
+        _currentDirection = currentDirection;
+    }
+    return self;
+}
 
 @end
